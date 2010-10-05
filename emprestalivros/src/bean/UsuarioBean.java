c9c42ca480f0ed74935966c6fa37dad0059711b0
fgalluzzo <fgalluzzo@8072cc2b-14fc-fcf8-d0b7-4586b2da6f7c>
2010-10-05 12:14:40 +0000

    emprestalivros para tomcat 6

---
+++ emprestalivros/src/bean/UsuarioBean.java
@@ -0,0 +1,86 @@
+package bean;
+
+import java.util.Date;
+
+import javax.annotation.Generated;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="usuario")
+public class UsuarioBean {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name="nome")
+	private String nome; 
+	
+	@Column(name="apelido")
+	private String apelido;
+	
+	@Column(name="senha")
+	private String senha;
+	
+	@Column(name="data_nascimento")
+	private Date dt_nascimento;
+	
+	@Column(name="email")
+	private String email;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getApelido() {
+		return apelido;
+	}
+
+	public void setApelido(String apelido) {
+		this.apelido = apelido;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Date getDt_nascimento() {
+		return dt_nascimento;
+	}
+
+	public void setDt_nascimento(Date dt_nascimento) {
+		this.dt_nascimento = dt_nascimento;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+}
